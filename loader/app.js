f1d0a7bbdb9d39e79994e532220728782952f0ea
Andre LeFort <lefort.a@gmail.com>
2015-06-27 22:17:24 -0400

    no more to json

--- loader/app.js
+++ loader/app.js
@@ -280,5 +280,5 @@ function loadDataset(schema, fileName){
 //getDatasetsZip();
 //extractDatasetsZip();
 //loadDataset('stores', './data/stores.csv');
-//loadDataset('products', './data/products.json');
-loadDataset('inventories', './data/inventories.csv');
+loadDataset('products', './data/products.csv');
+//loadDataset('inventories', './data/inventories.csv');
