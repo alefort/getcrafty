f948a19bdc751b04fcbbc1c6c13a9cbefd2845b7
Scott <scott@tbkcreative.com>
2015-07-25 18:15:58 -0400

    add distance_from_me to the stores view

--- app/public/home/home.js
+++ app/public/home/home.js
@@ -30,11 +30,12 @@
           config = {};
 
         config.url = 'http://www.getcrafty.co:3000/api/v1/storesNear?lat=' + lat + '&long=' + lon;
-        $scope.locationData = {};
+        $scope.stores = {};
         responsePromise = $http(config);
 
         responsePromise.success(function(data, status, headers, config) {
-          $scope.locationData = data;
+          $scope.stores = data;
+          console.log(data);
         });
 
         responsePromise.error(function(data, status, headers, config) {
