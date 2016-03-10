1223ff603e9ed9d8ec73a750ae13aa94c1a4a7cd
Andre LeFort <lefort.a@gmail.com>
2016-03-09 22:06:45 -0500

    first pass at the is_new algorithm

--- post-loader-tasks/app.js
+++ post-loader-tasks/app.js
@@ -1,14 +1,10 @@
 "use strict";
 /**
- * LCBO API Data Loader
+ * LCBO API Data Loader - Post load tasks
  *
- * This loader is written to do full loads of the LCBOAPI.COM nightly datasets.
- * These datasets are in CSV format, the following loader is responsible for
- *  - Downloading a new dataset archive
- *  - Expanding the contents of the archives
- *  - Parsing the CSV Files and loading each record into MongoDB
+ * this simple task runner will process the data that was loaded via the LCBO API data loader
  *
- * Created by Andre LeFort on 2015-04-10.
+ * Created by Andre LeFort on 2016-02-16.
  */
 
 
@@ -28,29 +24,23 @@ var mongoose = require('mongoose'),
 var eventController = {
     init: function(){
         /* Lets setup the events we care about */
-        eventEmitter.on('download_data', lcboLoader.getDatasetsZip);
-        eventEmitter.on('extract_data', lcboLoader.extractDatasetsZip);
-        eventEmitter.on('load_dataset', lcboLoader.loadDataset);
-        eventEmitter.on('load_stores', lcboLoader.loadStores);
-        eventEmitter.on('load_products', lcboLoader.loadProducts);
-        eventEmitter.on('load_inventories', lcboLoader.loadInventories);
-        eventEmitter.on('exit', lcboLoader.terminate);
-        eventEmitter.on('load_dataset_complete', lcboLoader.datasetLoaded);
-        eventEmitter.on('start_dataload', lcboLoader.loadStores);
+        eventEmitter.on('begin_process', taskRunner.beginProcess);
+        eventEmitter.on('process_is_new_flag', taskRunner.processIsNewFlag);
+        eventEmitter.on('exit', taskRunner.terminate);
     }
 };
 
-var lcboLoader = {
+var taskRunner = {
     init: function(){
-        lcboLoader.mongo.db = mongoose.connect('mongodb://' + lcboLoader.config.mongo.host + '/' + lcboLoader.config.mongo.db);
-        lcboLoader.log('Successfully connected to Mongo server ' + mongoose.connection.host + ' on port ' + mongoose.connection.port);
+        taskRunner.mongo.db = mongoose.connect('mongodb://' + taskRunner.config.mongo.host + '/' + taskRunner.config.mongo.db);
+        taskRunner.log('Successfully connected to Mongo server ' + mongoose.connection.host + ' on port ' + mongoose.connection.port);
         mongoose.set('debug', false);
     },
     log: function(message){
-        console.log(lcboLoader.getDate() + ':  ' + message);
+        console.log(taskRunner.getDate() + ':  ' + message);
     },
     error: function(message){
-        console.error(lcboLoader.getDate() + ':  ' + message);
+        console.error(taskRunner.getDate() + ':  ' + message);
     },
     getDate: function(){
       return new Date().toString();
@@ -83,218 +73,48 @@ var lcboLoader = {
         db: ''
     },
     terminate: function(){
-        lcboLoader.log('Terminating data load process.');
+        taskRunner.log('Terminating task runner process.');
         process.exit(0);
     },
-    make_url_friendly_string: function(to_convert){
-        var url_friendly_string = to_convert.replace(/[^a-z0-9]/gi, '-').toLowerCase();
-        url_friendly_string = url_friendly_string.replace('--','-');
-
-        return url_friendly_string;
+    beginProcess: function(){
+        eventEmitter.emit('process_is_new_flag');
     },
-    processStoreRecord: function(storeRecord){
-        var doc = lcboLoader.mongo.store.model(storeRecord);
-        doc.location = {};
-        doc.location.latitude = doc.latitude;
-        doc.location.longitude = doc.longitude;
-        doc.url_friendly_name = lcboLoader.make_url_friendly_string(doc.name + '-' + doc.city);
+    processIsNewFlag: function(){
+        // We need to figure out if each beer at each location is considered 'new' or not.
+        var archives = taskRunner.mongo.inventory_archive.model;
 
-        doc.save(function(error, doc) {
-            if (error) {
-                lcboLoader.error(error);
-                process.exit(1);
-            }
+        //var store_stream = taskRunner.mongo.store.model.find({}).lean().stream();
+        //var beer_stream = taskRunner.mongo.product.model.find({primary_category: 'Beer'}).lean().stream();
+        var inventory_stream = taskRunner.mongo.inventory.model.find({}).lean().stream();
 
-            return;
-        });
-    },
-    processProductRecord: function(productRecord){
-        var doc = lcboLoader.mongo.product.model(productRecord);
+        var fivedaysAgo = new Date();
+        fivedaysAgo.setDate(fivedaysAgo.getDate() - 5);
 
-        doc.url_friendly_name = lcboLoader.make_url_friendly_string(doc.name + '-' + doc.id);
-
-        doc.save(function(error, doc) {
-            if (error) {
-                lcboLoader.error(error);
-                process.exit(1);
-            }
-
-            return;
-        });
-    },
-    processInventoryRecord: function(inventoryRecord){
-        var doc = lcboLoader.mongo.inventory.model(inventoryRecord);
-
-        doc.save(function(error, doc) {
-            if (error) {
-                lcboLoader.error(error);
-                process.exit(1);
-            }
-
-            return;
-        });
-    },
-    getDatasetsZip: function(){
-        var options = {url: 'http://' + lcboLoader.config.url + '/datasets/latest.zip'};
-        lcboLoader.log('Downloading file: ' + options.url);
-
-        http.get(options, config.loader.datapath + '/latest.zip', function (error, result) {
-            if (error) {
-                lcboLoader.error(error);
-                process.exit(1);
-            }
-
-            eventEmitter.emit('extract_data');
-        });
-    },
-    extractDatasetsZip: function(){
-        var unzipper = new zip(config.loader.datapath + '/latest.zip')
-
-        unzipper.on('error', function (error) {
-            lcboLoader.error('Error extracting zip: ' + error);
-            process.exit(1);
-        });
-
-        unzipper.on('extract', function (log) {
-            lcboLoader.log('Finished extracting files from archive.');
-
-            /* Let's emit the next event in the chain */
-
-            eventEmitter.emit('start_dataload');
-            return;
-        });
-
-        unzipper.on('progress', function (fileIndex, fileCount) {
-            /* */
-        });
-
-        unzipper.extract({
-            path: config.loader.datapath,
-            filter: function (file) {
-                return file.type !== "SymbolicLink";
-            }
-        });
-    },
-    convertTsFs: function(record){
-        for(var key in record){
-            if(record[key] == 't'){
-                record[key] = true;
-            }else if(record[key] == 'f'){
-                record[key] = false;
-            }
-        }
-
-        return record;
-    },
-    uppercaseData: function(record){
-        if(record.varietal != undefined && record.varietal != ''){
-            var varietal = record.varietal.replace(/\(([^)]+)\)/, function(_, style) { return '(' + style.toUpperCase() + ')'; });
-
-            record.varietal = varietal;
-        }
-
-        return record;
-    },
-    loadDataset: function(schema, schemaName, fileName, deleteQuery){
-        /* First let's wipe the collection clean */
-        schema.remove(deleteQuery, function(error){
-            if (error) {
-                lcboLoader.error(error);
-                process.exit(1);
-            }
-
-            var reader = csv.createCsvFileReader(fileName, {columnsFromHeader: true});
-
-            reader.addListener('data', function(data) {
-                var record = lcboLoader.convertTsFs(data);
-                record = lcboLoader.uppercaseData(data);
-
-                /*
-                No need to process records that are dead
-                 */
-                if(record.is_dead == true){
-                    return;
+        inventory_stream.on('data', function (doc) {
+            archives.findOne({
+                store_id: doc.store_id,
+                product_id: doc.product_id,
+                reported_on:{
+                    $lte: fivedaysAgo
                 }
-
-                if(schemaName == 'stores'){
-                    lcboLoader.processStoreRecord(record);
-                }else if (schemaName == 'products'){
-                    lcboLoader.processProductRecord(record);
-                }else if(schemaName == 'inventories') {
-                    lcboLoader.processInventoryRecord(record);
-                }
-            });
-
-            reader.addListener('end', function(data) {
-                lcboLoader.log('Finished loading data for "' + schemaName + '" from ' + fileName);
-                /*
-                Lets let the handler know we're done this file
-                 */
-                eventEmitter.emit('load_dataset_complete', schemaName);
-            });
-        });
-    },
-    loadStores: function(){
-        eventEmitter.emit('load_dataset', lcboLoader.mongo.store.model, 'stores', config.loader.datapath + '/stores.csv', {});
-    },
-    loadProducts: function(){
-        eventEmitter.emit('load_dataset', lcboLoader.mongo.product.model, 'products', config.loader.datapath + '/products.csv', {});
-    },
-    loadInventories: function(){
-        // Need to copy the data over to inventories archive collection before pruning and loading below
-        var oneMonthAgo = new Date();
-        oneMonthAgo.setDate(oneMonthAgo.getDate() -30);
-
-        var date_query = {updated_at: {$lt: oneMonthAgo}};
-        // Prune the archives to only keep one month of data
-        var model = lcboLoader.mongo.inventory_archive.model;
-        model.remove(date_query, function(error){
-            var stream = lcboLoader.mongo.inventory.model.find({}).lean().stream();
-
-            stream.on('data', function (doc) {
-                var archived_inventory = {};
-                archived_inventory.product_id = doc.product_id;
-                archived_inventory.store_id = doc.store_id;
-                archived_inventory.is_dead = doc.is_dead;
-                archived_inventory.quantity = doc.quantity;
-                archived_inventory.reported_on = doc.reported_on;
-                archived_inventory.updated_at = doc.updated_at;
-
-                var archive_doc = lcboLoader.mongo.inventory_archive.model(archived_inventory);
-
-                archive_doc.save(function(error, doc) {
-                    if (error) {
-                        lcboLoader.error(error);
-                        process.exit(1);
-                    }
-
-                    return;
+            }).exec(function(err, record){
+                console.log(doc);
+                doc.is_new = true;
+                doc = taskRunner.mongo.inventory_archive.model(doc);
+                doc.save(function(err, doc){
+                    // Non critical, let it fail silently
                 });
-            }).on('error', function (err) {
-                lcboLoader.error(err);
-                process.exit(1);
-            }).on('close', function () {
-                eventEmitter.emit('load_dataset', lcboLoader.mongo.inventory.model, 'inventories', config.loader.datapath + '/inventories.csv', {});
             });
+        }).on('error', function (err) {
+            taskRunner.error(err);
+            process.exit(1);
+        }).on('close', function () {
+
         });
-    },
-    datasetLoaded: function(schemaName){
-        switch(schemaName){
-            case 'stores':
-                lcboLoader.loadProducts();
-                break;
-            case 'products':
-                lcboLoader.loadInventories();
-                break;
-            case 'inventories':
-                eventEmitter.emit('exit');
-                break;
-        }
     }
 };
 /* Final Bootstrap */
 eventController.init();
-lcboLoader.init();
+taskRunner.init();
 /* Now, let's kick off the work */
-eventEmitter.emit('download_data');
-//lcboLoader.loadStores();
+eventEmitter.emit('begin_process');
